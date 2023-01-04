06df8f28d84632c0e843f0a65a6e354c36b73ec5
OrangeRootkit <katkovdv88@gmail.com>
2023-01-04 18:59:26 +0300

    update

--- script.js
+++ script.js
@@ -2,19 +2,18 @@ const burger = document.querySelector("#burger"); // передаем конст
 const burger__list = document.getElementById("#burger__list"); // передаем константе селектор по id значок контейнер меню со ссылками
 const cross = document.querySelector("#cross");
 
-
 burger.onclick = function () {
-     burger.classList.add("active");
-     cross.classList.add("active");
-     burger__list.classList.add("active");
-}
+  burger.classList.add("active");
+  cross.classList.add("active");
+  burger__list.classList.add("active");
+};
 //      menu.classList.add("active"); // добавляем меню по нажатию
 //      burger.classList.add("active");// скрываю значок бургера
 //      // cross.classList.add("active");// добавляю крестик
 // }
 
 cross.onclick = function () {
-     burger__list.classList.remove("active"); // убираю класс с меню по нажатию - скрываю
-     burger.classList.remove("active");// возвращаю бургер
-     cross.classList.remove("active");// убираю крестик
-}
+  burger__list.classList.remove("active"); // убираю класс с меню по нажатию - скрываю
+  burger.classList.remove("active"); // возвращаю бургер
+  cross.classList.remove("active"); // убираю крестик
+};
